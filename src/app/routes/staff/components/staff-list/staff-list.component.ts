69d4ca439dab9f40ded12308e23d7cfe013caa25
Lai Manith <manith0302@gmail.com>
2022-09-01 20:50:52 +0700

    adding func print pdf in staff list

--- src/app/routes/staff/components/staff-list/staff-list.component.ts
+++ src/app/routes/staff/components/staff-list/staff-list.component.ts
@@ -11,6 +11,10 @@ import { SnackbarComponent } from 'src/app/shares/snackbar/components/snackbar/s
 import { MonthPipe } from 'src/app/shares/static-month/pipe/month.pipe';
 import * as XLSX from 'xlsx';
 
+import * as pdfMake from '../../../../../../node_modules/pdfmake/build/pdfmake.js';
+import * as pdfFonts from 'src/assets/fonts/vfs_fonts';
+(<any>pdfMake).vfs = pdfFonts.pdfMake.vfs;
+
 @Component({
   selector: 'app-staff-list',
   templateUrl: './staff-list.component.html',
@@ -138,10 +142,247 @@ export class StaffListComponent implements OnInit {
   }
 
   export(): void {
-    this.excelService.exportAsExcelFile(this.staffs, 'staff-list');
+    this.onPrint();
+    //this.excelService.exportAsExcelFile(this.staffs, 'staff-list');
   }
 
   customMonth(date: Date) {
     return formatDate(new Date(date), 'dd-MM-yyyy', 'en-US');
+  }
+
+
+  mapTableBody(data: any[]): Array<Array<string>> {
+    let tbData: any[];
+    tbData = [['លេខរៀង', 'ឈ្មោះ', 'ភេទ', 'ពិន្ទុចុងឆ្នាំ', 'អវត្តមានសរុប', 'លទ្ធផលចុងឆ្នាំ']];
+    for (let i: number = 0; i < data.length; i++) {
+      const tempRow = [];
+      tempRow.push(i + 1);
+      tempRow.push(data[i].name);
+      tempRow.push(data[i].gender);
+      tempRow.push(data[i].year_average);
+      tempRow.push(data[i].attendances);
+      tempRow.push(data[i].pass_fail);
+
+      tbData.push(tempRow);
+    }
+    return tbData;
+  }
+
+  onPrint() {
+    const cWidths = [];
+    cWidths.push('auto');
+    cWidths.push('*');
+    cWidths.push('auto');
+    cWidths.push('auto');
+    cWidths.push('auto');
+    cWidths.push('auto');
+
+    let dataRender = this.staffs;
+
+    let dataBody = this.mapTableBody(dataRender);
+    let styledData = dataBody.map((rowValue, rIndex) =>
+      dataBody[rIndex].map((columnValue, cIndex) => {
+
+        //Sample Data to return
+        let dataReturn = {
+          text: '',
+          color: '#424242',
+          margin: [10, 5, 10, 5],
+          alignment: 'center',
+          fillColor: ''
+        };
+
+        if (rIndex === 0) {
+          dataReturn.text = columnValue;
+          dataReturn.color = '#000';
+          dataReturn.fillColor = '#D3E4DD';
+          return dataReturn;
+        }
+        else if (cIndex === 0) {
+          dataReturn.text = columnValue;
+          dataReturn.color = '#237a57';
+          if (rIndex % 2 === 0) {
+            dataReturn.fillColor = '#e8f1ee';
+            return dataReturn;
+          }
+          else {
+            return dataReturn;
+          }
+        }
+
+        if (rIndex % 2 === 0) {
+          if (+columnValue === 1) {
+            dataReturn.color = '#13A200';
+            dataReturn.text = 'ជាប់';
+            dataReturn.fillColor = '#e8f1ee';
+            return dataReturn;
+          }
+          else if (+columnValue === -1) {
+            dataReturn.text = 'ធ្លាក់';
+            dataReturn.color = '#F44336';
+            dataReturn.fillColor = '#e8f1ee';
+            return dataReturn;
+          }
+          else {
+            dataReturn.text = columnValue;
+            dataReturn.fillColor = '#e8f1ee';
+            return dataReturn;
+          }
+        }
+        else {
+          if (+columnValue === -1) {
+            dataReturn.text = 'ធ្លាក់';
+            dataReturn.color = '#F44336';
+            return dataReturn;
+          }
+          else if (+columnValue === 1) {
+            dataReturn.color = '#13A200';
+            dataReturn.text = 'ជាប់';
+            return dataReturn;
+          }
+          else {
+            dataReturn.text = columnValue;
+            return dataReturn;
+          }
+        }
+      })
+    );
+
+    let pdfTitle: string = "ព័ត៌មានថ្នាក់ និងចំនួនសិស្ស";
+    let classHeader: string = "ព័ត៌មានទូទៅក្នុងថ្នាក់";
+    let dataHeader = {
+      academicYear: 2022,
+      grade: 'test',
+      name: 'test',
+      classroom: 'test',
+      homeroomTeacher: 'test',
+      status: 1 === 1 ? { name: "សកម្ម", name_en: "active" } : { name: "អសកម្ម", name_en: "inactive" }
+    }
+    let tableHeader: string = "តារាងឈ្មោះសិស្សក្នុងថ្នាក់"
+
+    const DATA = {
+      pageSize: 'A4',
+      content: [
+        {
+          text: pdfTitle, style: ['header', 'alignCenter']
+        },
+        {
+          text: classHeader,
+          style: 'sectionHeader'
+        },
+        {
+          columns: [
+            [
+              { text: `ឆ្នាំសិក្សា: ${dataHeader.academicYear}` },
+              { text: `ថ្នាក់: ${dataHeader.grade}` },
+              { text: `ឈ្មោះ:  ${dataHeader.name}` },
+              { text: `ថ្នាក់រៀន: ${dataHeader.classroom}` },
+            ],
+            [
+              {
+                text: `គ្រូបន្ទុកថ្នាក់: ${dataHeader.homeroomTeacher}`,
+                alignment: 'right'
+              },
+              {
+                text: `វេនសិក្សាបច្ចុប្បន្ន:  —/—`,
+                alignment: 'right'
+              },
+              {
+                alignment: 'right',
+                text: [
+                  {
+                    text: 'ស្ថានភាព:  '
+                  },
+                  {
+                    style: dataHeader.status.name_en,
+                    text: dataHeader.status.name,
+                  }
+                ]
+              }
+            ]
+          ]
+        },
+        {
+          text: '\n'
+        },
+        {
+          text: tableHeader,
+          style: 'sectionHeader'
+        },
+        {
+          style: 'tableGrading',
+          table: {
+            widths: cWidths,
+            headerRows: 1,
+            body: styledData
+          },
+          layout: {
+            hLineWidth: function (i, node) {
+              return 0;
+            },
+            vLineWidth: function (i, node) {
+              return 0;
+            },
+          }
+        }
+      ],
+      defaultStyle: {
+        font: 'Battambang',
+        fontSize: 11
+      },
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+          margin: [0, 0, 0, 25]
+        },
+        subheader: {
+          fontSize: 16,
+          bold: true,
+        },
+        tableHeader: {
+          bold: true,
+          fontSize: 12,
+          color: 'white'
+        },
+        alignCenter: {
+          alignment: 'center'
+        },
+        ml15: {
+          margin: [-15, 0, 0, 0]
+        },
+        tableGrading: {
+          color: '#424242',
+        },
+        sectionHeader: {
+          decoration: 'underline',
+          bold: true,
+          fontSize: 12,
+          margin: [0, 0, 0, 10]
+        },
+        active: {
+          color: '#13A200'
+        },
+        inactive: {
+          color: '#F44336'
+        }
+      }
+    };
+    pdfMake.fonts = {
+      Roboto: {
+        normal: 'Roboto-Regular.ttf',
+        bold: 'Roboto-Medium.ttf',
+        italics: 'Roboto-Italic.ttf',
+        bolditalics: 'Roboto-MediumItalic.ttf'
+      },
+      Battambang: {
+        normal: 'Battambang-Regular.ttf',
+        bold: 'Battambang-Bold.ttf',
+        italics: 'Battambang-Regular.ttf',
+        bolditalics: 'Battambang-Regular.ttf'
+      }
+    };
+
+    pdfMake.createPdf(DATA).print();
   }
 }
