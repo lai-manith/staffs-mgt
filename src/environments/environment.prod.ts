fbe9d0126156c83ab7c750ed85e52bca27788151
Manith <92358459+lai-manith@users.noreply.github.com>
2024-05-22 23:08:39 -0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url: "https://bgjmhw8kb3.execute-api.ap-southeast-1.amazonaws.com/dev/api",
-  file_static_url: "https://staffmgt.s3-accelerate.amazonaws.com/"
+  api_url: "https://staff-mgt-api.vercel.app/api",
+  file_static_url: "https://staff-mgt-api.vercel.app/api"
 };
